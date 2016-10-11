3ea343f0ab97764ec4ab93f2b7847746e2ae3ec5
Tony Brix <tbrix13@gmail.com>
2016-10-11 13:45:05 -0500

    update pictures

--- index.php
+++ index.php
@@ -232,12 +232,12 @@ mixpanel.init("ec2a03d0a0d066d969951d2d2ecffde3");</script>
 		<div data-href="https://www.linkedin.com/pub/tony-brix/37/436/98a/" data-img="images/linkedin.png" id="linkedin" class="shortcut"><span class="title">LinkedIn</span></div>
 		<div data-img="images/minesweeper.png" id="minesweeper" class="desktop shortcut"><span class="title">Mine Sweeper</span></div>
 		<div id="minesweepercontent"><canvas id="minesweepercanvas" tabindex="0">Your browser is not compatible.Please update your browser.</canvas></div>
-		<!--div id="pwalg" data-img="images/pwalg.png" class="desktop shortcut"><span class="title">Password Algorithm</span></div>
-		<div id="pwalgcontent" style="background-color: #fff;overflow: hidden !important"><iframe style="width: 100%;height: 9999999px;max-height: 100%;" src="https://uzitech.com/pwalg"></iframe></div-->
-		<div id="mobilepwalg" data-href="https://uzitech.com/pwalg" data-img="images/pwalg.png" class="shortcut"><span class="title">Password Algorithm</span></div>
-		<div data-href="https://uzitech.com/pws" id="pws" data-img="images/pws.png" class="desktop shortcut"><span class="title">Password Keys</span></div>
-		<!--div id="pwscontent" style="background-color: #fff;overflow: hidden !important"><iframe style="width: 100%;height: 9999999px;max-height: 100%;" src="//uzitech.com/pws"></iframe></div-->
-		<div id="mobilepws" data-href="https://uzitech.com/pws" data-img="images/pws.png" class="mobile shortcut"><span class="title">Password Keys</span></div>
+		<!-- <div id="pwalg" data-img="images/pwalg.png" class="desktop shortcut"><span class="title">Password Algorithm</span></div> -->
+		<!-- <div id="pwalgcontent" style="background-color: #fff;overflow: hidden !important"><iframe style="width: 100%;height: 9999999px;max-height: 100%;" src="https://uzitech.com/pwalg"></iframe></div> -->
+		<!-- <div id="mobilepwalg" data-href="https://uzitech.com/pwalg" data-img="images/pwalg.png" class="shortcut"><span class="title">Password Algorithm</span></div> -->
+		<!-- <div data-href="https://uzitech.com/pws" id="pws" data-img="images/pws.png" class="desktop shortcut"><span class="title">Password Keys</span></div> -->
+		<!-- <div id="pwscontent" style="background-color: #fff;overflow: hidden !important"><iframe style="width: 100%;height: 9999999px;max-height: 100%;" src="//uzitech.com/pws"></iframe></div> -->
+		<!-- <div id="mobilepws" data-href="https://uzitech.com/pws" data-img="images/pws.png" class="mobile shortcut"><span class="title">Password Keys</span></div> -->
 		<div id="googleplay" data-href="https://play.google.com/store/apps/developer?id=UziTech" data-img="images/googleplay.png" class="shortcut"><span class="title">Google Play</span></div>
 		<div id="github" data-href="https://github.com/uzitech" data-img="images/github.png" class="shortcut"><span class="title">GitHub</span></div>
 		<div id="gists" data-href="https://gist.github.com/uzitech" data-img="images/gist.png" class="shortcut"><span class="title">Gists</span></div>
@@ -261,57 +261,43 @@ mixpanel.init("ec2a03d0a0d066d969951d2d2ecffde3");</script>
 
 	<div id="loadingimages">
 		<div id="tonysimgs">
-			<img src='images/tony/tractor.jpg' data-width='480' data-height='360' alt="Tony Brix" />
-			<img src='images/tony/cherryberry.jpg' data-width='344' data-height='324' alt="Tony Brix" />
-			<img src='images/tony/holloween.jpg' data-width='694' data-height='690' alt="Tony Brix" />
-			<img src='images/tony/wedding.jpg' data-width='429' data-height='340' alt="Tony Brix" />
-			<img src='images/tony/zoo.jpg' data-width='573' data-height='540' alt="Tony Brix" />
-			<img src='images/tony/553081_10100414409267861_2143091519_n.jpg' data-width='549' data-height='549' alt="Tony Brix" />
-			<img src='images/tony/1021037_10100570919041021_1112411182_o.jpg' data-width='800' data-height='600' alt="Tony Brix" />
-			<img src='images/tony/24441_327944307319300_797827848_n.jpg' data-width='600' data-height='600' alt="Tony Brix" />
-			<img src='images/tony/29588_10100670098459671_1091024644_n.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/64872_125210684329157_750889273_n.jpg' data-width='960' data-height='632' alt="Tony Brix" />
-			<img src='images/tony/198363_10100257416617391_1439535719_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/205866_874456680201_1295628_n.jpg' data-width='514' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/255322_10100280354419851_1432915869_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/293066_10100295497133731_175293267_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/302079_911400559371_223851350_n.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/302435_911409970511_431662050_n.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/305205_911404157161_1239905019_n.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/307660_10200307950120112_1209088522_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/380645_10100285846842991_689066883_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/391508_10100310138946421_1766732978_n.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/393679_10150512984038776_698187792_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/398612_10100409666063281_379422769_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/430549_10100352383178641_1266775155_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/485519_10151664078690769_1485142210_n.jpg' data-width='852' data-height='639' alt="Tony Brix" />
-			<img src='images/tony/532273_10100280342603531_2123524387_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/557088_4090531585994_1871747651_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/579671_10100670098070451_1205447995_n.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/599702_10100409659855721_2131006437_n.jpg' data-width='720' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/912972_10100530036589781_1857428578_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/942327_10100527719233781_2143157115_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/945859_10100527733345501_215275619_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/968021_10100900810959971_799730891_n.jpg' data-width='792' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/983258_10100570919041021_1112411182_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/1080844_10100618284425411_1390283153_n.jpg' data-width='720' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/1185671_10100670097556481_1556234586_n.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/1233402_10100670103105361_774514018_n.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/1239441_10100670098250091_926823134_n.jpg' data-width='640' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/1290030_10100658459000241_859920189_n.jpg' data-width='720' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/1391996_10202464451831307_672921890_n.jpg' data-width='960' data-height='540' alt="Tony Brix" />
-			<img src='images/tony/1502092_10100794733964121_1115007217_n.jpg' data-width='720' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/1509138_10203492736337777_1556803074_n.jpg' data-width='960' data-height='540' alt="Tony Brix" />
-			<img src='images/tony/1528062_10100797008126681_1436562687_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/1899154_10100842819570181_1282608400_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/1902773_10203492807579558_1017966438_n.jpg' data-width='960' data-height='540' alt="Tony Brix" />
-			<img src='images/tony/10168947_10100900818100661_250843300_n.jpg' data-width='670' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/10169001_10100900812955971_485816978_n.jpg' data-width='720' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/10178313_10100900843454851_1614577081_n.jpg' data-width='960' data-height='834' alt="Tony Brix" />
-			<img src='images/tony/10255462_10100900817506851_1382073500_n.jpg' data-width='813' data-height='960' alt="Tony Brix" />
-			<img src='images/tony/10261806_10100900780605801_1949900553_n.jpg' data-width='960' data-height='720' alt="Tony Brix" />
-			<img src='images/tony/IMG_0532.jpg' data-width='960' data-height='640' alt="Tony Brix" />
-			<img src='images/tony/facebook_1422485401377.jpg' data-width='960' data-height='640' alt="Tony Brix" />
+			<img src='images/tony/Tony1.jpg' data-width='960' data-height='540' alt="Tony Brix" />
+			<img src='images/tony/Tony2.jpg' data-width='960' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony3.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony4.jpg' data-width='540' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony5.jpg' data-width='528' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony6.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony7.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony8.jpg' data-width='819' data-height='505' alt="Tony Brix" />
+			<img src='images/tony/Tony9.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony10.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony11.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony12.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony13.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony14.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony15.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony16.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony17.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony18.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony19.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony20.jpg' data-width='528' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony21.jpg' data-width='960' data-height='632' alt="Tony Brix" />
+			<img src='images/tony/Tony22.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony23.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony24.jpg' data-width='852' data-height='639' alt="Tony Brix" />
+			<img src='images/tony/Tony25.jpg' data-width='429' data-height='340' alt="Tony Brix" />
+			<img src='images/tony/Tony26.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony27.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony28.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony29.jpg' data-width='720' data-height='960' alt="Tony Brix" />
+			<img src='images/tony/Tony30.jpg' data-width='960' data-height='540' alt="Tony Brix" />
+			<img src='images/tony/Tony31.jpg' data-width='960' data-height='540' alt="Tony Brix" />
+			<img src='images/tony/Tony32.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony33.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony34.jpg' data-width='960' data-height='540' alt="Tony Brix" />
+			<img src='images/tony/Tony35.jpg' data-width='960' data-height='834' alt="Tony Brix" />
+			<img src='images/tony/Tony36.jpg' data-width='960' data-height='720' alt="Tony Brix" />
+			<img src='images/tony/Tony37.jpg' data-width='960' data-height='640' alt="Tony Brix" />
 		</div>
 	</div>
 </body>
