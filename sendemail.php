9a974d115a02e974968a6c5ab6448643fc1cfd51
Tony Brix <tbrix13@gmail.com>
2016-11-10 21:50:11 -0600

    mail works

--- sendemail.php
+++ sendemail.php
@@ -2,14 +2,23 @@
 if (isset($_POST["from"], $_POST["subject"], $_POST["body"])) {
 	$return = ["success" => true];
 	$ipaddress = $_SERVER["REMOTE_ADDR"];
+	$userAgent = $_SERVER["HTTP_USER_AGENT"];
+	$browser = get_browser();
+	$from = $_POST["from"];
 
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-	$headers .= 'From: '.$_POST["from"];
-	$body = $_POST["body"];
-	$body = str_replace("href=\"/", "href=\"http://tonybrix.info/", $body);
-	$body = str_replace("src=\"/", "src=\"http://tonybrix.info/", $body);
-	if (!mail("tony@brix.ninja", "Tony.Brix.ninja: ".$_POST["subject"], $body, $headers)) {
+	$headers  = "MIME-Version: 1.0\r\n";
+	$headers .= "Content-type: text/html; charset=utf-8\r\n";
+	$headers .= "From: tbrix13+contact@gmail.com\r\n";
+	$headers .= "Reply-To: {$from}\r\n";
+	$body = "<p>Email: {$from}</p>";
+	$body .= "<p>IP Address: {$ipaddress}</p>";
+	$body .= "<p>UA: {$userAgent}</p>";
+	$body .= "<p>Message:</p>";
+	$body .= $_POST["body"];
+	$body = preg_replace("/((href|src)\s*=\s*[\"'])\//i", "$1https://tony.brix.ninja/", $body);
+	$subject = "Message from tony.brix.ninja: {$_POST["subject"]}";
+
+	if (!mail("tony@brix.ninja", $subject, $body, $headers)) {
 		$return = [
 			"success" => false,
 			"error" => "There has been an error sending the email. If you need to contact me you may send an email to Tony@Brix.ninja at any time.",
