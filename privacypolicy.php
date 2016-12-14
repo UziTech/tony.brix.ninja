f502b250706488767858c348fc3a63eb862c886c
Tony Brix <tbrix13@gmail.com>
2016-12-14 17:08:34 -0600

    add privacypolicy and termsofuse

---
+++ privacypolicy.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tony Brix: Privacy Policy</title>
+  <meta charset="utf-8">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width">
+<style>
+body {
+  font-family: monospace;
+  /*
+  color: #fff;
+  background-color: #000;
+  */
+}
+</style>
+</head>
+<body>
+  <h1>PRIVACY POLICY</h1>
+  <p>This privacy policy discloses the privacy practices for HTTPS://TONY.BRIX.NINJA. This privacy notice applies solely to information collected by this web site. It will notify you of the following:</p>
+  <p>What personally identifiable information is collected from you through the web site, how it is used and with whom it may be shared.</p>
+  <p>The security procedures in place to protect the misuse of your information.</p>
+  <p>Information Collection, Use, and Sharing.</p>
+  <p>We are the sole owners of the information collected on this site. We only have access to collect information that you voluntarily give us via email or other direct contact from you. We will not sell or rent this information to anyone.</p>
+  <p>We will use your information to respond to you, regarding the reason you contacted us. We will not share your information with any third party outside of our organization, other than as necessary to fulfill your request, e.g. to ship an order.</p>
+  <h2>Security</h2>
+  <p>We take precautions to protect your information. When you submit sensitive information via the website, your information is protected both online and offline.</p>
+  <h2>Links</h2>
+  <p>This web site contains links to other sites. Please be aware that we are not responsible for the content or privacy practices of such other sites. We encourage our users to be aware when they leave our site and to read the privacy statements of any other site that collects personally identifiable information.</p>
+  <h2>If you feel that we are not abiding by this privacy policy, you should contact us immediately via telephone at 320-249-1820.</h2>
+</body>
+</html>
---
+++ termsofuse.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tony Brix: Privacy Policy</title>
+  <meta charset="utf-8">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width">
+<style>
+body {
+  font-family: monospace;
+}
+</style>
+</head>
+<body>
+  <h1>TERMS OF USE</h1>
+  <p>The usual: <b>I'm not responible for anything</b> and <b>don't do anything illegal.</b></p>
+  <p>If you don't like it, you can leave at anytime.</p>
+  <h2>All rights reserved. All wrongs denied.</h2>
+</body>
+</html>
